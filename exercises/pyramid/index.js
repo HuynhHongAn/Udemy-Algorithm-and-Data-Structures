feedf1bc3d4f1f9501c8c76d390a5b83502bc927
1512014 <1512014@student.hcmus.edu.vn>
2019-06-13 18:08:36 +0700

    Finished pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,34 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid01(n) {
+    let lineLength = n * 2 - 1;
+    for (let i = 0; i < n; i++){ //Vertical
+        let midIndex = n - 1;
+        let line = '';
+        for (let j = 0; j < lineLength; j++){
+            if (j >= midIndex - i && j <= midIndex + i){
+                line += '#';
+            }
+            else
+                line += ' ';
+        }
+        console.log(line);
+    }
+}
+
+//recursive pyramid
+function pyramid(n, row = 0, line = ''){
+    if (row === n)
+        return;
+
+    if (line.length === 2*n - 1){
+        console.log(line);
+        return pyramid(n, row + 1);
+    }
+
+    let add = (line.length <= (n - 1) + row && line.length >= (n - 1) - row)? '#':' ';
+    pyramid(n, row, line + add);
+}
 
 module.exports = pyramid;
