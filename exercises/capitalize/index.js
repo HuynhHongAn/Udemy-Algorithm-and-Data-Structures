55f6d0ab0bc79c8b196c57da9ead7a58d16f6a6e
1512014 <1512014@student.hcmus.edu.vn>
2019-06-13 17:28:26 +0700

    Finished capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    let result = '';
+    result += str[0].toUpperCase();
+    for (let i = 1; i < str.length; i++){
+        if (str[i-1] === ' '){
+            result += str[i].toUpperCase();
+        }
+        else
+            result += str[i];
+    }
+    return result;
+}
+
+function capitalize01(str) {
+    let strArr =  str.split(' ');
+    let resultArr = [];
+    strArr.forEach((word) => {
+            var temp = word[0].toUpperCase() + word.slice(1);
+            resultArr.push(temp);
+        });
+    return resultArr.join(' ');
+}
 
 module.exports = capitalize;
