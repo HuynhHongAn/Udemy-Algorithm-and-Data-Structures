a7dac815046ea9a3c313b9a925496adecfca8d9c
1512014 <1512014@student.hcmus.edu.vn>
2019-06-14 16:26:38 +0700

    Finished matrix

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -14,7 +14,49 @@
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
+function squareZeros(dim) {
+    let result = [];
+    let dim1 = dim;
+    let dim2 = dim;
+    while (dim1){
+        let tempArr = [];
+        while (dim2){
+            tempArr.push(0);
+            dim2--;
+        }
+        result.push(tempArr);
+        dim1--;
+    }
+    return result;
+}
 
-function matrix(n) {}
+function matrix(n) {
+    let padding = 0;
+    let count = 1;
+    let result = squareZeros(n);
+    while (count <= n*n){
+        for (let i = padding, j = padding; j < n-1-padding; j++){
+            result[i][j] = count;
+            count++;
+        }
+        for (let i = padding, j = n-1-padding; i< n-1-padding; i++){
+            result[i][j] = count;
+            count++;
+        }
+        for (let i = n-1-padding, j = n-1-padding; j > padding; j--){
+            result[i][j] = count;
+            count++;
+        }
+        for (let i = n-1-padding, j = padding; i > padding; i--){
+            result[i][j] = count;
+            count++;
+        }
+        padding ++;
+    }
+    debugger;
+    return result;
+}
+
+matrix(4);
 
 module.exports = matrix;
