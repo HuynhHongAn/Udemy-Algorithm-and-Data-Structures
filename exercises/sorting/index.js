af8b3bc605806e8369cb526630834c051a1c803a
1512014 <1512014@student.hcmus.edu.vn>
2019-06-18 18:00:24 +0700

    Complete selection sort & bubble sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,11 +2,32 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+    for (let i = 0; i < arr.length; i++){
+        for (let j = 0; j < arr.length - i - 1; j++){
+            if (arr[j] >= arr[j+1]){
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
 }
 
 function selectionSort(arr) {
 
+    for (let i = 0; i< arr.length - 1; i++){
+        let indexOfMin = i;
+        for (let j = i+1; j < arr.length; j++){
+            if (arr[j] < arr[indexOfMin]){
+                indexOfMin = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[indexOfMin];
+        arr[indexOfMin] = temp;
+    }
+    return arr;
 }
 
 function mergeSort(arr) {
